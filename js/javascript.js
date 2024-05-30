cadefdd8bf9a2028b54a083893fbd9b68a706f95
Aakash yadav <126560106+aakashy5990@users.noreply.github.com>
2024-05-30 16:12:51 +0530

    Updated

--- js/javascript.js
+++ js/javascript.js
@@ -1,45 +1,52 @@
 const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
+        const listContainer = document.getElementById("list-container");
 
-function addTask(){
-    if(inputBox.value === ''){
-        alert("You must write something");
-    }
-    else{
-        let li = document.createElement("li")
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
+        function addTask(){
+            if(inputBox.value === ''){
+                alert("You must write something");
+            }
+            else{
+                let li = document.createElement("li");
+                li.innerHTML = inputBox.value;
+                listContainer.appendChild(li);
 
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7"
-        li.appendChild(span);
+                let span = document.createElement("span");
+                span.innerHTML = "\u00d7";
+                li.appendChild(span);
 
-        span.addEventListener("click", function(){
-            li.remove();
+                span.addEventListener("click", function(){
+                    li.remove();
+                    saveData();
+                });
+            }
+
+            inputBox.value = "";
             saveData();
+        }
+
+        inputBox.addEventListener("keydown", function(event){
+            if (event.key === 'Enter') {
+                addTask();
+            }
         });
-    }
-    
-    inputBox.value = "";
-    saveData();
-}
 
-listContainer.addEventListener("click",function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-        saveData();
-    }
-    else if(e.target.tagName === "span"){
-        e.target.parentElement.remove();
-        saveData();
-    }
+        listContainer.addEventListener("click", function(e){
+            if(e.target.tagName === "LI"){
+                e.target.classList.toggle("checked");
+                saveData();
+            }
+            else if(e.target.tagName === "SPAN"){
+                e.target.parentElement.remove();
+                saveData();
+            }
+        }, false);
 
-},false)
+        function saveData(){
+            localStorage.setItem("data", listContainer.innerHTML);
+        }
 
-function saveData(){
-    localStorage.setItem("data",listContainer.innerHTML);
-}
-function showTask(){
-    listContainer.innerHTML = localStorage.getItem("data");
-}
-showTask();
+        function showTask(){
+            listContainer.innerHTML = localStorage.getItem("data");
+        }
+
+        showTask();
